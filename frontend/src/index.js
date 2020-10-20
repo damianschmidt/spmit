62cda138c5f5fd2b8c120aded742c42545712e9c
Aleksander Atamaczuk <aleksander.atamanczuk@gmail.com>
2020-10-20 20:18:59 +0200

    build: create react app

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Sterowanie Produkcją Magazynową i Transportową</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
