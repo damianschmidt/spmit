8cb7ed248bbe0b052f60e1de88e680e6db845153
Schmidt Damian <damian.schmidt@nokia.com>
2020-11-14 13:22:50 +0100

    feature: add api usage for package lists

--- backend/api/package_lists.py
+++ backend/api/package_lists.py
@@ -1,3 +1,4 @@
+import json
 from flask import Blueprint, jsonify, request
 from marshmallow import ValidationError
 
@@ -18,14 +19,17 @@ def get_package_lists():
     return jsonify(package_lists_names), 200
 
 
-@PACKAGE_LISTS.route('/<package_list>', methods=['GET'])
-def get_package_list(package_list):
+@PACKAGE_LISTS.route('/<package_list_name>', methods=['GET'])
+def get_package_list(package_list_name):
     package_lists_db = PackageListsDbTools()
     package_lists = package_lists_db.get_package_lists()
 
-    lockers = []
-    # TODO: filter lockers names from package list
-    return jsonify(lockers), 200
+    for package_list in package_lists:
+        if package_list_name == package_list['name']:
+            return jsonify(package_list['package_list']), 200
+
+    msg = f'List {package_list_name} not found in database'
+    return jsonify(msg), 404
 
 
 @PACKAGE_LISTS.route('', methods=['POST'])
@@ -40,17 +44,20 @@ def add_package_list():
     list_file = request.files['file']
     if list_file.filename == '':
         return jsonify({'error': 'No file selected for uploading'}), 400
-    return jsonify(list_file.filename), 200
 
+    data = json.load(list_file)
 
-    # try:
-    #     data_dict = ADD_PACKAGE_LIST_SCHEMA.load(request.get_json())
-    # except ValidationError as error:
-    #     return jsonify({'error': str(error)}), 400
+    try:
+        data_dict = {
+            "name": list_file.filename[:-5],
+            "package_list": data['package_list']
+        }
+    except KeyError as error:
+        return jsonify({'error': 'Wrong file format'})
 
-    # package_lists_db = PackageListsDbTools()
-    # package_lists_db.add_user(data_dict)
-    # return jsonify(data_dict), 200
+    package_lists_db = PackageListsDbTools()
+    package_lists_db.add_package_list(data_dict)
+    return jsonify(data_dict), 200
 
 
 @PACKAGE_LISTS.route('', methods=['DELETE'])
--- frontend/src/components/PackagesList.jsx
+++ frontend/src/components/PackagesList.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import FormData from "form-data";
-import fs from "fs";
 import {
   Button,
   Form,
