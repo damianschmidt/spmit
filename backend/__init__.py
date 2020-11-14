af8da6bc732b849aa1786fbd6b1f223f5e7d3142
Schmidt Damian <damian.schmidt@nokia.com>
2020-11-14 12:00:57 +0100

    refactor: add blueprint for package_lists

--- backend/__init__.py
+++ backend/__init__.py
@@ -17,10 +17,12 @@ def create_app(config_class=Config):
     from backend.api.here_api import HERE_API
     from backend.api.lockers import LOCKERS
     from backend.api.users import USERS
+    from backend.api.package_lists import PACKAGE_LISTS
 
     app.register_blueprint(BASIC, url_prefix='/api/1')
     app.register_blueprint(HERE_API, url_prefix='/api/1/here_api')
     app.register_blueprint(LOCKERS, url_prefix='/api/1/lockers')
     app.register_blueprint(USERS, url_prefix='/api/1/users')
+    app.register_blueprint(PACKAGE_LISTS, url_prefix='/api/1/package_lists')
 
     return app
